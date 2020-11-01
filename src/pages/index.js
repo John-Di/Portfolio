c5e99f11e3deb661ca93c718c443119e39548461
John <john.luke.digirolamo+git@gmail.com>
2020-11-01 17:24:37 -0500

    Alright I think I found the outlier finally. However, there might be an error because there's the /resume template that creates a page and then there's the "createPages" that creates the /resume page, but I might have a solution.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,9 @@
 import React from "react"
-import { css } from "@emotion/core"
-import SEO from "../components/seo"
-import PageLayout from "../layouts/resume-layout"
-
-import { rhythm } from "../utils/typography"
-import Basics from "../components/resume/basics"
-import About from "../components/resume/about"
+import Resume from "../components/resume";
 
 
 export default function ResumePage({ data, pageContext }) {
-	let { basics } = pageContext.resume;
-
 	return (
-		<PageLayout>
-			<SEO title={`Resume`} description={basics.name} />
-			<Basics data={data} pageContext={pageContext} />
-			<About data={data} pageContext={pageContext} />
-		</PageLayout>
+		<Resume data={data} pageContext={pageContext} />
 	)
 }
