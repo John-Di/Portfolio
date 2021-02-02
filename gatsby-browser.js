37357690b006ec21babf5295ba67d880712dc39b
John Di Girolamo <57693075+John-Di@users.noreply.github.com>
2021-02-01 21:55:40 -0500

    Added a fix for the SVGs being huge on load. Like honestly. Why do I need to manually do this lol???

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,6 @@
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
 import "./src/styles/global.css"
+
+// You should do that in a Layout file or in `gatsby-browser.js`.
+config.autoAddCss = false;
