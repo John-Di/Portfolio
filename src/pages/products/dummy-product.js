e82192c0a4962591becff6375d8dd4b1164f805a
John Di Girolamo <57693075+John-Di@users.noreply.github.com>
2021-02-22 21:02:14 -0500

    Moved the product defaults to a const.

--- src/pages/products/dummy-product.js
+++ src/pages/products/dummy-product.js
@@ -9,14 +9,12 @@ import {
 import ProductPage from '../../layouts/product-page';
 // import ImageGallerySection from "../../sections/image-gallery-section";
 
-
-// markup
-const DummyProduct = ({
-  title = 'Dummy Product',
-  price = '$19.99',
-  description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-  images = randomImageArray(),
-  options = [{
+const DUMMY_PRODUCT = {
+  title: 'Dummy Product',
+  price: '$19.99',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  images: randomImageArray(),
+  options: [{
     "name": "Color",
     "colors": [
       "white", "black", "#708090", "#2F4F4F"
@@ -27,18 +25,14 @@ const DummyProduct = ({
       "xs", "s", "m", "l", "xl"
     ]
   }]
-}) => {
+};
+
+// markup
+const DummyProduct = () => {
   let accentColor = randomColor();
 
   return (
-    <ProductPage
-      images={images}
-      accentColor={accentColor}
-      title={title}
-      price={price}
-      description={description}
-      options={options}
-    />
+    <ProductPage {...DUMMY_PRODUCT} accentColor={accentColor} />
   )
 }
 
