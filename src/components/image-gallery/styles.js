ad358b53e115ebdc1956f4d6596b081251c5a8f0
John Di Girolamo <57693075+John-Di@users.noreply.github.com>
2021-02-22 21:06:01 -0500

    Removed z-index on thumbnail vignette effect because it was overlaying even over the header which is unnecessary.

--- src/components/image-gallery/styles.js
+++ src/components/image-gallery/styles.js
@@ -7,9 +7,9 @@ import {
 import FullSizeOverlay from '../../utils/FullSizeOverlay';
 
 export const WRAPPER = styled.div`
-	height: 100%;
-	width: 100%;
-	margin: 0 auto;
+  height: 100%;
+  width: 100%;
+  margin: 0 auto;
 
   ${props => conditionalProp(props.hasPadding, `
     padding: 2% 0;
@@ -19,80 +19,80 @@ export const WRAPPER = styled.div`
     }
   `)}
 
-	display: flex;
-	flex-flow: column nowrap;
-	justify-content: center;
-	align-items: center;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
   ${props => conditionalProp(props.maxWidth, `
     max-width: ${size.mobileXL + 'px'};
   `)}
 `;
 
 export const GALLERY = styled.div`
-	display: flex;
-	flex-flow: column nowrap;
-	justify-content: center;
-	margin: auto;
-	width: 100%;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  margin: auto;
+  width: 100%;
 
-	${Clearfix}
-	@supports not (display:grid) {
-	}
+  ${Clearfix}
+  @supports not (display:grid) {
+  }
 `;
 
 export const NAVIGATION = styled.div`
   margin-top: 0.75em;
 
-	${Clearfix}
+  ${Clearfix}
 
-	@supports not (display:grid) {
-	}
+  @supports not (display:grid) {
+  }
 `;
 
 export const MAIN_IMAGE = styled.div`
-	grid-area: a;
-	position: relative;
+  grid-area: a;
+  position: relative;
   background-color: grey;
 
-	&::before {
-		content: '';
-		display: block;
-		padding-top: 100%;
-		width: 100%;
-	}
+  &::before {
+    content: '';
+    display: block;
+    padding-top: 100%;
+    width: 100%;
+  }
 `;
 
 export const SCROLLABLE = styled.div`
-	overflow: auto;
-	white-space: nowrap;
+  overflow: auto;
+  white-space: nowrap;
 
-	-ms-overflow-style: none;  /* IE and Edge */
-	scrollbar-width: none;  /* Firefox */
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none;  /* Firefox */
 
-	&::-webkit-scrollbar {
-		display: none;
-	}
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 export const THUMBNAIL = styled.button`
-	margin: 0;
-	padding: 0;
-	border: 0;
-	outline: none;
-	background: none;
-	margin: 0;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  outline: none;
+  background: none;
+  margin: 0;
   float: left;
-	max-width: ${props => props.maxWidth}%;
-	display: inline-block;
-	vertical-align: bottom;
-	width: 100%;
-	overflow: hidden;
-	position: relative;
+  max-width: ${props => props.maxWidth}%;
+  display: inline-block;
+  vertical-align: bottom;
+  width: 100%;
+  overflow: hidden;
+  position: relative;
 
-	@media ${device.laptop} {
-		margin: 0;
-		max-width: ${props => props.maxWidth}%;
-	}
+  @media ${device.laptop} {
+    margin: 0;
+    max-width: ${props => props.maxWidth}%;
+  }
 
   &:not(.current) {
     cursor: pointer;
@@ -105,29 +105,28 @@ export const THUMBNAIL = styled.button`
     }
   }
 
-	&:not(.current) img {
-		${props => props.transparency ? `opacity : ${props.transparency};` : ''}
-	}
+  &:not(.current) img {
+    ${props => props.transparency ? `opacity : ${props.transparency};` : ''}
+  }
 
 
   &::after {
     box-shadow: inset 0.125em 0.125em 1em black;
   }
 
-	&::before {
+  &::before {
     content: '';
     display: block;
     background-color: slategrey;
     padding-top: 100%;
     width: 100%;
   }
-
-	&::after {
-		content: '';
+  
+  &::after {
+    content: '';
     ${FullSizeOverlay}
-		display: block;
-    z-index: 1;
-	}
+    display: block;
+  }
 
   &.current {
     &,
@@ -148,18 +147,18 @@ export const THUMBNAIL = styled.button`
 `;
 
 export const IMG = styled.img`
-	display: block;
-	max-width: 100%;
-	vertical-align: bottom;
-	width: 100%;
-	position: absolute;
-	top: 0;
-	left: 0;
-	right: 0;
-	bottom: 0;
-	margin: 0;
-	height: 100%;
-	transition: transform 0.15s ease-in 0s, opacity 0.15s ease-in 0s;
+  display: block;
+  max-width: 100%;
+  vertical-align: bottom;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: 0;
+  height: 100%;
+  transition: transform 0.15s ease-in 0s, opacity 0.15s ease-in 0s;
 
   .current & {
     transform: scale(1.25);
