f5b551e72dcec00d0aae3c1ee070afba851ad8b6
John Di Girolamo <57693075+John-Di@users.noreply.github.com>
2021-06-12 08:10:58 -0400

    Finally transitioning the button style for the "chic" theme to match the text color transition.

--- src/components/chic-cta/styles.js
+++ src/components/chic-cta/styles.js
@@ -50,7 +50,7 @@ const ButtonStyles = `
   text-align: center;
   text-transform: uppercase;
   letter-spacing: 0.2px;
-  transition: background-color 0.25s 0s, color 0.25s 0s;
+  transition: background-color 0.25s 0s, color 0.25s 0s, border-color 0.25s 0s;
 `
 
 export const BUTTON = styled.button`
